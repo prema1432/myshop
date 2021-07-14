6df7f13de0a3299f14896b50b926533cc0909d7d
prema <talamarlapremanath@gmail.com>
2021-07-14 15:42:54 +0530

    shop app

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200,db_index=True)
     slug = models.SlugField(max_length=200,unique=True)
@@ -12,6 +15,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    #adding
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
@@ -30,3 +37,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    #adding
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',args=[self.id, self.slug])
